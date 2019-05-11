d004e38bebf46a4deb7a0d9d38529074c335fa6a
Yan Laguna <yan.laguna@gmail.com>
2019-05-11 16:17:15 -0300

    improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,23 @@
 puts 'Creating Dog Walks'
 
-100.times {
-  DogWalk.create!(
-    title: Faker::Lorem.sentence,
-    scheduled_datetime: Faker::Time.between(2.days.ago, Date.today, :all)
-  )
-}
+ActiveRecord::Base.transaction do
+  100.times do
+    dog_walk = DogWalk.create(
+      title: Faker::Lorem.sentence,
+      scheduled_datetime: Faker::Time.between(2.days.ago, 2.days.from_now, :all)
+    )
 
-puts 'Done'
+    rand(1..3).times do
+      DogWalkPet.create(
+        name: Faker::Creature::Dog.name,
+        race: Faker::Creature::Dog.breed,
+        observation: Faker::Creature::Dog.meme_phrase,
+        age: rand(0..20),
+        dog_walk: dog_walk
+      )
+    end
+  end
+
+end
+
+puts 'DONE'
